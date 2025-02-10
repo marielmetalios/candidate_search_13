b81c05226a37eb7d69a9819ad55f6a32cf6eb8c8
Your Name <marielmetalios@gmail.com>
2025-02-10 15:26:41 -0500

    updating .env and initial interfaces

--- src/interfaces/Candidate.interface.tsx
+++ src/interfaces/Candidate.interface.tsx
@@ -1,1 +1,8 @@
 // TODO: Create an interface for the Candidate objects returned by the API
+interface Candidate {
+    Name: string,
+    Location: string,
+    Email: string,
+    Company: string,
+    Bio: string
+}
