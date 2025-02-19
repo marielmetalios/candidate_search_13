84ba00abcff0ae1a91904e719aeb76334a020481
Your Name <marielmetalios@gmail.com>
2025-02-19 07:46:53 -0500

    updated CandidateSearch page, image successfully pulling into table

--- src/pages/SavedCandidates.tsx
+++ src/pages/SavedCandidates.tsx
@@ -1,39 +1,39 @@
 const SavedCandidates = () => {
 
-  
-
+  const candidates = JSON.parse(localStorage.getItem("candidates") as string) || [];
+  console.log(candidates);
 
   return (
     <>
-    <div>
-      <h2>Potential Candidates</h2>
-      <table>
-        <thead>
-          <tr>
-            <th>Image</th>
-            <th>Name</th>
-            <th>Location</th>
-            <th>Email</th>
-            <th>Company</th>
-            <th>Bio</th>
-            <th>Reject</th>
-          </tr>
-      </thead>
-      <tbody>
-        {/* candidates.map.forEach({candidate}) */}
-        <th>Image</th>
-            <th>placeholder</th>
-            <th>placeholder</th>
-            <th>placeholder</th>
-            <th>placeholder</th>
-            <th>placeholder</th>
-            <th>placeholder</th>
-      </tbody>
-      </table>
-
-
-
-    </div>
+      <div>
+        <h2>Potential Candidates</h2>
+        <table>
+          <thead>
+            <tr>
+              <th>Image</th>
+              <th>Name</th>
+              <th>Location</th>
+              <th>Email</th>
+              <th>Company</th>
+              <th>Bio</th>
+              <th>Reject</th>
+            </tr>
+          </thead>
+          <tbody>
+            {candidates.map((candidate, index) => (
+              <tr key={candidate.login}>
+            <td><img src={candidate.avatar_url}/></td>
+            <td>{candidate.name}</td>
+            <td>{candidate.location}</td>
+            <td>{candidate.email}</td>
+            <td>{candidate.company}</td>
+            <td>{candidate.bio}</td>
+            <td>placeholder</td>
+            </tr>
+          ))}
+          </tbody>
+        </table>
+      </div>
     </>
   );
 };
