fa5130a93d3ebf716e6b06d672350f4443e73553
Your Name <marielmetalios@gmail.com>
2025-02-19 07:25:26 -0500

    updating table structure on SavedCandidates

--- src/pages/SavedCandidates.tsx
+++ src/pages/SavedCandidates.tsx
@@ -1,8 +1,39 @@
 const SavedCandidates = () => {
+
+  
+
+
   return (
     <>
-      <h1>Potential Candidates</h1>
-      get
+    <div>
+      <h2>Potential Candidates</h2>
+      <table>
+        <thead>
+          <tr>
+            <th>Image</th>
+            <th>Name</th>
+            <th>Location</th>
+            <th>Email</th>
+            <th>Company</th>
+            <th>Bio</th>
+            <th>Reject</th>
+          </tr>
+      </thead>
+      <tbody>
+        {/* candidates.map.forEach({candidate}) */}
+        <th>Image</th>
+            <th>placeholder</th>
+            <th>placeholder</th>
+            <th>placeholder</th>
+            <th>placeholder</th>
+            <th>placeholder</th>
+            <th>placeholder</th>
+      </tbody>
+      </table>
+
+
+
+    </div>
     </>
   );
 };
